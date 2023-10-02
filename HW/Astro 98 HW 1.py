3375f919c727d6e4b834be6227225157fe528f65
Levi Galvan II <125097497+LeviCodehub@users.noreply.github.com>
2023-10-01 22:13:41 -0700

    Add files via upload

---
+++ HW/Astro 98 HW 1.py
@@ -0,0 +1,7 @@
+#This is the amount of life I have left because of school :(           
+Original_amount_of_life = 9.89483e10      
+life_from_hw =  -7.9853e8           
+time_for_myself_in_hours = 1.1345e-2
+the_will_to_continue = 0
+life_i_have_left = ((Original_amount_of_life+life_from_hw)*time_for_myself_in_hours)*the_will_to_continue
+print(life_i_have_left)
